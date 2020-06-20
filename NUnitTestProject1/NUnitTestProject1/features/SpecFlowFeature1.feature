f7f3ebead718012f27def58183dd7f2b4fc51491
GAURAV <gauravmudgalnsw@gmail.com>
2020-06-21 00:24:20 +1000

    My first sample test project

---
+++ NUnitTestProject1/NUnitTestProject1/features/SpecFlowFeature1.feature
@@ -0,0 +1,15 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario Outline: Open youtube and gmail
+	Given I have entered <URL1> into the browser
+	And I have entered <URL2> into the browser
+	When I press add
+	Then the result should be 120 on the screen
+Examples: 
+
+| URL1              | URL2                |
+| https://gmail.com | https://youtube.com |
---
+++ NUnitTestProject1/NUnitTestProject1/steps/SpecFlowFeature1Steps.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium.Support.UI;
+
+namespace NUnitTestProject1.steps
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the browser")]
+        public void GivenIHaveEnteredIntoTheCalculator(string url)
+        {
+            //ScenarioContext.Current.Pending();
+                     
+            IWebDriver webDriver = new ChromeDriver(@"C:\SpecflowProjects\NUnitTestProject1\NUnitTestProject1\driver\");
+            //string baseURL = "https://gmail.com";
+            webDriver.Navigate().GoToUrl(url);
+            //webDriver.Navigate("www.youtube.com");
+            webDriver.Close();
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd() =>
+            //ScenarioContext.Current.Pending();
+            Console.WriteLine("how are you");
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+           // ScenarioContext.Current.Pending();
+            Console.WriteLine("how are you");
+        }
+    }
+}
